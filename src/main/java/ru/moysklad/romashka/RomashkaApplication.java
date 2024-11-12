62cf9d946843043f14e40bf8ba143e188fbeccc1
Sergey Shadrin <juuute3110@gmail.com>
2024-11-12 22:24:28 +0700

    first commit

---
+++ src/main/java/ru/moysklad/romashka/RomashkaApplication.java
@@ -0,0 +1,13 @@
+package ru.moysklad.romashka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomashkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomashkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/moysklad/romashka/RomashkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.moysklad.romashka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomashkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
